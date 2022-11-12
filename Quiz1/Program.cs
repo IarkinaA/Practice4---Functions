8e93c03831620a738b0d9f9f2ad19ab7ddbf32eb
eiiti <eiiti@mail.ru>
2022-11-12 14:44:58 +0300

    Practice4

---
+++ Quiz1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
+// в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число В");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Getnum (int number)
+{
+    int result = 1;
+    for(int i = 1; i<=num2; i++)
+    {
+        result = result*num1;
+    }
+    return result;
+}
+
+Console.WriteLine("Возведение чисел равно" + Getnum(num1));
+
+
+
+
---
+++ Quiz2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int count = a;
+int GetSum(int a)
+{
+    int x = 0;
+    while(a>0)
+    {
+        x++;
+        a/=10;
+    }
+
+    return x;
+} 
+Console.WriteLine("Количество цифр равно"+" "+GetSum(a));
+
